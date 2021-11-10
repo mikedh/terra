3993af34fba132f95d05e8c36c24c1e6e103a4a5
Michael Dawson-Haggerty <mikedh@kerfed.com>
2021-11-10 14:38:34 -0500

    only run windows to test

---
+++ windows.bat
@@ -0,0 +1,30 @@
+
+curl -OL "https://github.com/%BINARY_SOURCE_USER%/llvm-package-windows/releases/download/clang-%LLVM_VERSION%-nvptx/llvm-%LLVM_VERSION%-windows-amd64-msvc%VS_MAJOR_VERSION%-msvcrt.7z"
+
+7z x llvm-%LLVM_VERSION%-windows-amd64-msvc%VS_MAJOR_VERSION%-msvcrt.7z
+
+set LLVM_DIR=%CD%\llvm-%LLVM_VERSION%-windows-amd64-msvc%VS_MAJOR_VERSION%-msvcrt
+set CLANG_RESOURCE_DIRECTORY=%LLVM_DIR%\lib\clang\%LLVM_VERSION%
+
+set CMAKE_PREFIX_PATH=%LLVM_DIR%
+
+:: TODO : re-enable cude if
+if /I "%USE_CUDA%" EQU "1" (curl -L -o cuda.exe "https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers2/cuda_9.2.148_windows")
+if /I "%USE_CUDA%" EQU "1" (.\cuda -s nvcc_9.2 cudart_9.2)
+if /I "%USE_CUDA%" EQU "1" (set CUDA_DIR=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.2)
+
+set TERRA_DIR=%CD%
+
+ps: git describe --tags | ForEach-Object { $_ -replace "release-", "" } | Set-Content terra_version.txt
+set /p TERRA_VERSION=<terra_version.txt
+
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=%CD%\..\install -DCMAKE_GENERATOR_PLATFORM=x64
+cmake --build . --target INSTALL --config Release
+cd ..
+
+set TERRA_SHARE_PATH=%TERRA_DIR%\install\share\terra
+
+cd tests
+..\install\bin\terra run
+cd ..
