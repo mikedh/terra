d1e74a01086e978ed8b657acaa47093580b5c549
Michael Dawson-Haggerty <mikedh@kerfed.com>
2021-11-10 18:19:07 -0500

    remove ps

--- windows.bat
+++ windows.bat
@@ -15,7 +15,7 @@ if /I "%USE_CUDA%" EQU "1" (set CUDA_DIR=C:\Program Files\NVIDIA GPU Computing T
 
 set TERRA_DIR=%CD%
 
-ps: git describe --tags | ForEach-Object { $_ -replace "release-", "" } | Set-Content terra_version.txt
+git describe --tags | ForEach-Object { $_ -replace "release-", "" } | Set-Content terra_version.txt
 set /p TERRA_VERSION=<terra_version.txt
 
 cd build
